8eabaeceeb9b2d8bb25f01cdbdc4c8a2e22ebcb3
Jungma <mbjun3630@naver.com>
2023-06-17 15:52:32 +0900

    스프링 부트 설정

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -7,10 +7,12 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
+@Commit
 @Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
