abfd28b40c83fa01aedfb39eb37c11a053d118d6
Jungma <mbjun3630@naver.com>
2023-06-17 14:58:22 +0900

    Querydsl 설정과 검증

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,36 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+    @PersistenceContext
+    EntityManager em;
+
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(result);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
