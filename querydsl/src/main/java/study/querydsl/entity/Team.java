f7ae84bf1aa0c568fdd725e68446079fc3a5d1dc
Jungma <mbjun3630@naver.com>
2023-06-18 16:11:15 +0900

    예제 도메인 모델

---
+++ querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,35 @@
+package study.querydsl.entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(of = {"id", "name"})
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
